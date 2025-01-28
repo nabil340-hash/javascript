22800ed0f05c162d20722c32cb8d6f9538cd5f82
nabil340-hash <nabil.daif@yschools.eu>
2025-01-28 11:26:36 +0000

    3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+/*
 const text = "Creer une fonction qui analyse un texte et retourne des statistiques de base.";
 function trouverMotLePlusLong(text) {
     const mots = text.match(/\b\w+\b/g);
@@ -11,3 +11,28 @@ function trouverMotLePlusLong(text) {
     return motLePlusLong;
 }
 console.log(trouverMotLePlusLong(text));
+
+
+let primeur = {
+    fruits: ["fraise","bannane","kiwi"],
+    legumes: ["carrote", "patate", "courgette"]
+};
+console.log(primeur);
+console.log(typeof primeur);
+console.log(primeur.legumes[2]);
+
+*/
+//Boucler sur un tableau:
+fruits.forEach(function (item, index, array) {
+  console.log(item, index);
+});
+
+//Ajouter à la fin du tableau:
+let newLength = fruits.push("Orange");
+// ["Apple", "Banana", "Orange"]
+
+// Supprimer un élément par son index:
+let removedItem = fruits.splice(pos, 1); // supprime 1 élément à la position pos
+// ["Strawberry", "Mango"]
+
+faire des prompt et lui dire d'aller me chercher le livre et lui indiquer le stock 
